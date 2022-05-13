ad612b38628d0f3c798a7248269e4c94f150e1f8
Andrii Reznichenko <4ezeronnik@gmail.com>
2022-05-13 22:16:33 +0300

    added changes

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,26 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
+import SimpleLightbox from "simplelightbox";
+// Дополнительный импорт стилей
+import "simplelightbox/dist/simple-lightbox.min.css";
+
+const galleryContainer = document.querySelector('.gallery');
+const galleryMarkup = createGalleryMarkup(galleryItems);
+
+galleryContainer.insertAdjacentHTML('beforeend', galleryMarkup);
+
+// galleryContainer.addEventListener('click', onGalleryContainerClick);
+
+
+function createGalleryMarkup(galleryItems) {
+  return galleryItems.map(({preview, original, description}) => {
+    return `<a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" />
+</a>
+`;
+  }).join('');
+}
+const ligthBox = new SimpleLightbox('.gallery a', { captionsData: 'alt', captionDelay: 250, });
 
 console.log(galleryItems);
