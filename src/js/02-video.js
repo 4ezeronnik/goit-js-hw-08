68f34d71c86f78471f1971a0fba77500d803b8a9
Andrii Reznichenko <4ezeronnik@gmail.com>
2022-05-17 22:07:17 +0300

    Update 02-video.js

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,17 +1,15 @@
 import Player from '@vimeo/player';
 
 const iframe = document.querySelector('iframe');
-      const player = new Player(iframe);
-
-      player.on('play', function () {
-        console.log('played the video!');
-      });
+const player = new Player(iframe);
+      
+const onPlay = function (data) {
+    localStorage.setItem('videoplayer-current-time', JSON.stringify(data));
+};
+player.on('timeupdate', onPlay);
 
       player.getVideoTitle().then(function (title) {
         console.log('title:', title);
       });
 
-
-
-
-
+	
