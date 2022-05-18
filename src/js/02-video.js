aed5e314e8f46d798e396811075bcb882f69a2d3
Andrii Reznichenko <4ezeronnik@gmail.com>
2022-05-18 09:59:59 +0300

    Update 02-video.js

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -3,8 +3,10 @@ import Player from '@vimeo/player';
 const iframe = document.querySelector('iframe');
 const player = new Player(iframe);
       
+
 const onPlay = function (data) {
-    localStorage.setItem('videoplayer-current-time', JSON.stringify(data));
+  const timeData = JSON.stringify(data);
+  localStorage.setItem('videoplayer-current-time', timeData);
 };
 player.on('timeupdate', onPlay);
 
@@ -12,4 +14,16 @@ player.on('timeupdate', onPlay);
         console.log('title:', title);
       });
 
-	
+  player.setCurrentTime().then(function(seconds) {
+    // seconds = the actual time that the player seeked to
+}).catch(function(error) {
+    switch (error.name) {
+        case 'RangeError':
+            // the time was less than 0 or greater than the video’s duration
+            break;
+
+        default:
+            // some other error occurred
+            break;
+    }
+});
