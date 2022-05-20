4feeaaca7bbfb925c138b5aa6752d7f2381ec94d
Andrii Reznichenko <4ezeronnik@gmail.com>
2022-05-20 22:44:59 +0300

    Update 03-feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,20 @@
+// const inputRef = document.querySelector('input');
+// const textareaRef = document.querySelector('textarea');
+const formRef = document.querySelector('form');
 
+
+formRef.addEventListener('input', onInput);
+
+function onInput(evt) {
+    const message = evt.target.value;
+    localStorage.setItem('feedback-form-state', message);
+    console.log(message);
+}
+
+formRef.addEventListener('submit', onFormSubmit);
+
+function onFormSubmit(evt) {
+    evt.preventDefault();
+
+    evt.currentTarget.reset();
+}
