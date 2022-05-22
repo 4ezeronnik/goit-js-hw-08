4f924a3d4b3d571420afda51719d9c511c6c9dc4
Andrii Reznichenko <4ezeronnik@gmail.com>
2022-05-22 22:31:35 +0300

    Update 03-feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,7 +1,6 @@
 import throttle from "lodash.throttle";
 
 const STORAGE_KEY = 'feedback-form-state';
-const formData = JSON.parse(localStorage.getItem(STORAGE_KEY)) || {}; 
 
 
 const inputRef = document.querySelector('input');
@@ -14,20 +13,20 @@ formRef.addEventListener('input', throttle(onInput, 500));
 formRef.addEventListener('submit', onFormSubmit);
 
 function onInput(evt) {
-    
-    localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
+    const formData = JSON.parse(localStorage.getItem(STORAGE_KEY)) || {};
+   
     formData[evt.target.name] = evt.target.value;
- 
+     localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
+    console.log(formData);
 }
 
 function onFormSubmit(evt) {
     evt.preventDefault();
 
-    console.log(formData);
+    console.log(localStorage.getItem(STORAGE_KEY));
 
     evt.target.reset();
     localStorage.removeItem(STORAGE_KEY);
-    
     
 }
 
@@ -35,10 +34,9 @@ function populateTextarea() {
     const savedMessage = JSON.parse(localStorage.getItem(STORAGE_KEY));
     
     if (savedMessage) {
-    
+        
         inputRef.value = savedMessage.email || "";
         textareaRef.value = savedMessage.message || "";
-        
     }
 
 }
